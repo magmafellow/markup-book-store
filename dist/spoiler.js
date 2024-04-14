34b5f847f1e88f8b0d7bf320f06da91ef00106a7
magmafellow <magmafellow@gmail.com>
2024-04-14 17:03:04 +0300

    header, main done. footer needs to be completed

--- dist/spoiler.js
+++ dist/spoiler.js
@@ -1,6 +1,6 @@
-const spoiler1 = document.querySelector('.spoiler-1');
-const spoilerBtn1 = document.querySelector('.spoiler-1 .spoiler__btn')
+const spoiler1 = document.querySelector(".spoiler-1");
+const spoilerBtn1 = document.querySelector(".spoiler-1 .spoiler__btn");
 
-spoilerBtn1.addEventListener('click', function(e) {
-  spoiler1.classList.toggle('active')
-})
+spoilerBtn1.addEventListener("click", function (e) {
+  spoiler1.classList.toggle("active");
+});
